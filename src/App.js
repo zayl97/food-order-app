b42a492b7d43c263109b88fba1895914639dcde8
Zayl <kam.plociennik@gmail.com>
2021-11-25 16:11:52 +0100

    Created a layout for Meals List.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+
 import Header from "./components/Layout/Header/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlfor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  );
+};
+
+export default Input;
